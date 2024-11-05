7cc7b6f437cfcd02801fc9406eaf249cb19c23e7
Jude Maria Bevan <judeb@lolctech.com>
2024-11-06 00:43:47 +0530

    initial commit

---
+++ springboot/EventTicketingSystem/src/main/java/com/jd/EventTicketingSystem/config/Configuration.java
@@ -0,0 +1,19 @@
+package com.jd.EventTicketingSystem.config;
+
+public class Configuration {
+
+    private int totalTickets;
+
+    private int ticketReleaseRate;
+
+    private int customerRetrievalRate;
+
+    private int maxTicketCapacity;
+
+    public Configuration(int totalTickets, int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity) {
+        this.totalTickets = totalTickets;
+        this.ticketReleaseRate = ticketReleaseRate;
+        this.customerRetrievalRate = customerRetrievalRate;
+        this.maxTicketCapacity = maxTicketCapacity;
+    }
+}
---
+++ springboot/EventTicketingSystem/src/main/java/com/jd/EventTicketingSystem/controller/ConfigurationContoller.java
@@ -0,0 +1,4 @@
+package com.jd.EventTicketingSystem.controller;
+
+public class ConfigurationContoller {
+}
